2541f2dab73efcb19112ce4894ab0869396c251b
Carolina Saraiva Ferreira <75094303+carolasf@users.noreply.github.com>
2025-06-10 15:13:08 -0300

    selector objects added

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,21 +1,53 @@
-describe('Login com sucesso', () => {
+describe('Login Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[data-test='signin-username']",
+    passwordField: "[data-test='signin-password']",
+    loginButton: "[data-test='signin-submit']",
+    tabList: "[data-test='nav-transaction-tabs']",
+    wrongCredenntialAlert: "[role='alert']",
+
+  }
+
     it('Deve fazer login com usuário válido', () => {
       cy.visit('/signin')
-      cy.get('#username').type('Heath93')
-      cy.get('#password').type('s3cret')
-      cy.get('[data-test="signin-submit"]').click()
-      cy.url().should('include', "http://localhost:3000/")
+      cy.get(selectorsList.usernameField).type('Heath93')
+      cy.get(selectorsList.passwordField).type('s3cret')
+      cy.get(selectorsList.loginButton).click()
+      cy.get(selectorsList.tabList).should('be.visible')  
     })
 
-    it('Tentar fazer login com credenciais inválidas', () => {
+    it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
       cy.visit('/signin')
-      cy.get('#username').type('TestFail')
-      cy.get('#password').type('TestFail')
-      cy.get('[data-test="signin-submit"]').click()
-      cy.get("[role='alert']")
-    
-    })
-  
+      cy.get(selectorsList.usernameField).type('TestFail')
+      cy.get(selectorsList.passwordField).type('TestFail')
+      cy.get(selectorsList.loginButton).click()
+      cy.get(selectorsList.wrongCredenntialAlert) // Implemente os passos do caso de teste aqui
+    });
+
   })
+
   
+/*
+  describe('Registro de novo usuário com sucesso', () => {
+    it('Deve registrar um novo usuário com informações válidas', () => {
+      cy.visit('/signup')
+       // Preenche os campos do formulário
+    cy.get("#firstName").type('Carolina');
+    cy.get("#lastName").type('Ferreira');
+    cy.get('[data-test="username"]').type('carol_teste_' + Date.now()); // username único
+    cy.get('[data-test="password"]').type('SenhaSegura123!');
+    cy.get('[data-test="confirmPassword"]').type('SenhaSegura123!');
+
+    // Envia o formulário
+    cy.get('button[type="submit"]').click();
+
+    // Verifica se foi redirecionado para a página de login com sucesso
+    cy.url().should('include', '/signin');
+    cy.contains('Conta criada com sucesso').should('be.visible');
+  });
+});
+*/
+
+   
   
