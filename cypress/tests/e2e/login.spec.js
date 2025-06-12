7f3758e0f32d45a633e5af84d477b8b0a724587a
Carolina Saraiva Ferreira <75094303+carolasf@users.noreply.github.com>
2025-06-12 20:54:57 -0300

    Added dashboardPage, loginPage e signupPage

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,63 +1,60 @@
-import { last } from 'lodash';
 import userData from '../../fixtures/userData.json';
-import { sign } from 'crypto';
+import LoginPage from '../pages/loginPage.js';
+import SignupPage from '../pages/signupPage.js';
+import DashboardPage from '../pages/dashboardPage.js';
+
+
+const loginPage = new LoginPage()
+const signupPage = new SignupPage()
+const dashboardPage = new DashboardPage()
+
 
 describe('Login Tests', () => {
 
-  const selectorsList = {
-    usernameField: "[data-test='signin-username']",
-    passwordField: "[data-test='signin-password']",
-    loginButton: "[data-test='signin-submit']",
-    tabList: "[data-test='nav-transaction-tabs']",
-    wrongCredenntialAlert: "[role='alert']",
-    signupLink: "[data-test='signup']",
-    firstnameFieldSignup: "[data-test='signup-first-name']",
-    lastnameFieldSignup: "[data-test='signup-last-name']",
-    usernameFieldSignup: "[data-test='signup-username']",
-    passwordFieldSignup: "[data-test='signup-password']",
-    confirmPasswordFieldSignup: "[data-test='signup-confirmPassword']",
-    signupButton: "[data-test='signup-submit']",
+    it.only('Deve fazer login com usuário válido', () => {
+      //cy.intercept('POST', '/graphql').as('loginRequest');
 
 
+      loginPage.accessLoginPage();
+      loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password);
 
-  }
-
-
-    it('Deve fazer login com usuário válido', () => {
-      cy.visit('/signin')
-      cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-      cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-      cy.get(selectorsList.loginButton).click()
-      cy.get(selectorsList.tabList).should('be.visible')  
-    })
-
-    it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
-      cy.visit('/signin')
-      cy.get(selectorsList.usernameField).type(userData.userFail.username)
-      cy.get(selectorsList.passwordField).type(userData.userFail.password)
-      cy.get(selectorsList.loginButton).click()
-      cy.get(selectorsList.wrongCredenntialAlert) 
-    });
-
-    it('Deve registrar um novo usuário com informações válidas', () => {
-      cy.visit('/signin')
-      cy.get(selectorsList.signupLink).click();
-       // Preenche os campos do formulário
-      cy.get(selectorsList.firstnameFieldSignup).type('Carolina');
-      cy.get(selectorsList.lastnameFieldSignup).type('Ferreira');
-      cy.get(selectorsList.usernameFieldSignup).type('carol_teste_' + Date.now()); // username único
-      cy.get(selectorsList.passwordFieldSignup).type('SenhaSegura123!');
-      cy.get(selectorsList.confirmPasswordFieldSignup).type('SenhaSegura123!');
-  
-      // Envia o formulário
-      cy.get(selectorsList.signupButton).click();
-  
-      // Verifica se foi redirecionado para a página de login com sucesso
-      cy.url().should('include', '/signin');
+      dashboardPage.checkDashboardPage();
+      // cy.wait('@loginRequest').then((interception) => {
+      //   if (interception && interception.response) {
+      //     expect(interception.response.statusCode).to.eq(200);
+      //   } else {
+      //     throw new Error('A resposta da requisição de login está indefinida');
+      //   }
       });
-  })
 
+
+      });
+      
   
-   
-   
+
+      it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+        loginPage.accessLoginPage();
+        loginPage.loginWithUser(userData.userFail.username, userData.userFail.password);
+        loginPage.checkAccessInvalid();
+      });
+
+      it('Deve registrar um novo usuário com informações válidas', () => {
+        cy.visit('/signin')
+      
+         // Preenche os campos do formulário
+        cy.get(selectorsList.firstnameFieldSignup).type('Carolina');
+        cy.get(selectorsList.lastnameFieldSignup).type('Ferreira');
+        cy.get(selectorsList.usernameFieldSignup).type('carol_teste_' + Date.now()); // username único
+        cy.get(selectorsList.passwordFieldSignup).type('SenhaSegura123!');
+        cy.get(selectorsList.confirmPasswordFieldSignup).type('SenhaSegura123!');
+        // Envia o formulário
+        cy.get(selectorsList.signupButton).click();
+    
+        // Verifica se foi redirecionado para a página de login com sucesso
+        cy.url().should('include', '/signin');
+        });
   
+    
+   
+
+    
---
+++ cypress/tests/pages/dashboardPage.js
@@ -0,0 +1,16 @@
+    class DashboardPage {
+        selectorsList() {
+            const selectors = {
+                tabList: "[data-test='nav-transaction-tabs']",
+            }   
+            
+            return selectors;
+        }
+
+        checkDashboardPage() {
+            cy.location('pathname').should('eq', '/'); 
+            cy.get(this.selectorsList().tabList, { timeout: 10000 }).should('be.visible');
+        }
+    }
+
+    export default DashboardPage
